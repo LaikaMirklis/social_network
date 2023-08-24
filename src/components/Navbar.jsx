659c6856200676ac3544c6535ff2f48279b4c508
LaikaMirklis <olg.beam763@gmail.com>
2023-08-24 19:59:27 +0300

    separated styles into modules for each component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,21 @@
+import styles from './Navbar.module.css'
+
 const Navbar = () => {
     return (
-        <nav className="nav">
-            <div>
+        <nav className={styles.nav}>
+            <div className={styles.item}>
                 <a>Profile</a>
             </div>
-            <div>
+            <div className={`${styles.item} ${styles.active}`}>
                 <a>Messages</a>
             </div>
-            <div>
+            <div className={styles.item}>
                 <a>News</a>
             </div>
-            <div>
+            <div className={styles.item}>
                 <a>Music</a>
             </div>
-            <div>
+            <div className={styles.item}>
                 <a>Settings</a>
             </div>
         </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,16 @@
 import ava from "./avatar.png";
+import styles from "./Profile.module.css"
 
 const Profile = () => {
     return (
-        <div className="content">
+        <div className={styles.content}>
             <img
                 src="https://cdnb.artstation.com/p/assets/images/images/046/499/745/large/l-y-hyun-fantasy-forest-secret-village.jpg?1645264603"
-                className="background"
+                className={styles.background}
                 alt="background"
             />
             <div>
-                <img src={ava} className="avatar" alt="avatar" />
+                <img src={ava} className={styles.avatar} alt="avatar" />
                 ava + description
             </div>
             <div>
