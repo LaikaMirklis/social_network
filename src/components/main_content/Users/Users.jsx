8a24081c3138024e58b69962cfa22ada25447f3e
LaikaMirklis <olg.beam763@gmail.com>
2023-12-14 21:20:28 +0200

    l_53 functional  component Users => class component? add some styles to grid on Users page

--- src/components/main_content/Users/Users.jsx
+++ src/components/main_content/Users/Users.jsx
@@ -1,44 +1,44 @@
+import React from 'react';
 import styles from './Users.module.css'
 import User from './User/User';
 import axios from 'axios';
 
-const Users = (props) => {
-    const t = props.t;
-
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items)
-            })
-        }
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
 
+    render() {
+        const t = this.props.t;
 
-    let usersElements = props.users.map(u =>
-        <User
-            key={u.id}
-            id={u.id}
-            photos={u.photos}
-            followed={u.followed}
-            name={u.name}
-            status={u.status}
-            follow={props.follow}
-            unfollow={props.unfollow}
-            t={t}
-        />)
+        let usersElements = this.props.users.map(u =>
+            <User
+                key={u.id}
+                id={u.id}
+                photos={u.photos}
+                followed={u.followed}
+                name={u.name}
+                status={u.status}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+                t={t}
+            />)
 
-    return (
-        <div className={styles.findUsers}>
-            <button onClick={getUsers} className={styles.getUsersButton}>Get users</button>
-            <h3>{t('usersPage.users')}</h3>
-            <div className={styles.users}>
-                {usersElements}
+        return (
+            <div className={styles.findUsers}>
+                <h3>{t('usersPage.users')}</h3>
+                <div className={styles.users}>
+                    {usersElements}
+                </div>
+                <div className={styles.showMoreButtonWrapper}>
+                    <button className={styles.showMoreButton}>{t('usersPage.showMoreButton')}</button>
+                </div>
             </div>
-            <div className={styles.showMoreButtonWrapper}>
-                <button className={styles.showMoreButton}>{t('usersPage.showMoreButton')}</button>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Users;
