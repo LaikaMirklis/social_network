090dbd973fb78d77e43ceb2446247ea9affbf605
LaikaMirklis <olg.beam763@gmail.com>
2023-12-15 02:37:14 +0200

    added componentDidMount

--- src/components/main_content/Users/Users.jsx
+++ src/components/main_content/Users/Users.jsx
@@ -4,8 +4,8 @@ import User from './User/User';
 import axios from 'axios';
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             this.props.setUsers(response.data.items)
         })
